db3addc01ec7da0930c62dda6b69f99abba4b368
devkobby24 <nanakobby2002@gmail.com>
2024-10-21 14:33:37 +0000

    Hero Section

---
+++ components/Hero.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { Spotlight } from "./ui/Spotlight";
+import { ModeToggle } from "./ui/ModeToggle";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa";
+
+const Hero = () => {
+  return (
+    <div className="pb-20 pt-36">
+      <div>
+        <Spotlight
+          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+          fill="white"
+        />
+        <Spotlight
+          className="top-10 left-full w-[50vw] h-[80vh]"
+          fill="purple"
+        />
+        <Spotlight className="top-28 left-80 " fill="white" />
+        {/* <ModeToggle /> */}
+      </div>
+      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
+        {/* Radial gradient for the container to give a faded look */}
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+      </div>
+
+      <div className="flex justify-center relative my-20 z-10">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+            Welcome
+          </h2>
+          <TextGenerateEffect
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+            words="Transforming Concepts Into Seamless User Experiences"
+          />
+          <p className="text-center text-[40px] md:tracking-wider mb-4">
+            Hi I&apos;m Justice
+          </p>
+          <a href="#about">
+            <MagicButton
+              title="Show My Work "
+              icon={<FaLocationArrow />}
+              position="right"
+              handleClick={() => {}}
+              otherClasses="gap-2"
+            />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
