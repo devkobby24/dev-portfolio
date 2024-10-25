2de8091f44064d8ccd7bb81396260a2fa3581e10
devkobby24 <nanakobby2002@gmail.com>
2024-10-25 09:20:02 +0000

    Removed Sentry

---
+++ next.config.js
@@ -0,0 +1,10 @@
+// next.config.js
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true, // Use the default minifier
+  };
+  
+  module.exports = nextConfig;
+  
