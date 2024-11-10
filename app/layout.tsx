f1f97704206abe2976315eae660f5587294e7e60
devkobby24 <nanakobby2002@gmail.com>
2024-11-10 00:17:52 +0000

    Add SpeedInsights component and update package dependencies for performance monitoring

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "@/app/theme-provider";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -34,6 +35,7 @@ export default function RootLayout({
             defaultTheme="dark"
         >
         {children}
+        <SpeedInsights />
         </ThemeProvider>
       </body>
     </html>
