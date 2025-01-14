25e84f96281a98a6c452c09d8067249ad829175f
devkobby24 <nanakobby2002@gmail.com>
2025-01-14 20:44:32 +0000

    Update project title in gridItems to reflect current learning focus

--- data/index.ts
+++ data/index.ts
@@ -50,7 +50,7 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently building a prototype for a centralized EMR system",
+    title: "Currently learning about AI Agents",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
